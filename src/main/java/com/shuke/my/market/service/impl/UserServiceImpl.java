8c2123346974f4f8ea869801b461577fd5129764
ma-shucheng <shuchengma111@163.com>
2020-02-24 11:46:18 +0800

    增加业务层功能

--- src/main/java/com/shuke/my/market/service/impl/UserServiceImpl.java
+++ src/main/java/com/shuke/my/market/service/impl/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package com.shuke.my.market.service.impl;
 
 import com.shuke.my.market.dao.UserDao;
-import com.shuke.my.market.dao.impl.UserDaoImpl;
 import com.shuke.my.market.entity.User;
 import com.shuke.my.market.service.UserService;
 import org.springframework.context.ApplicationContext;
@@ -11,9 +10,18 @@ public class UserServiceImpl implements UserService {
 //    UserDao userDao = new UserDaoImpl();
 
     ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-context.xml");
+    UserDao userDao = (UserDao) applicationContext.getBean("userDao");
 
     public User login(String email, String loginPwd) {
-        UserDao userDao = (UserDao) applicationContext.getBean("userDao");
         return userDao.login(email,loginPwd);
     }
+
+    public Boolean remME(String remMe, String email, String loginPwd) {
+        return userDao.remME(remMe,email,loginPwd);
+    }
+
+    public Boolean autoSign(String autoSign, String email, String loginPwd) {
+        return userDao.autoSign(autoSign,email,loginPwd);
+    }
+
 }
